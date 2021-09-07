00e754f3b560558d7a53a8bdb123030dcca8033e
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-07 14:59:24 +0530

    ADDED: Method to Welcome the User

---
+++ src/com/bridgelabz/emailvalidator/EmailValidator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.emailvalidator;
+
+public class EmailValidator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("---------- Welcome To Email Validator ----------");
+	}
+
+}
